bab4afc83a0d46727abe53da703c7a215d464f8f
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-10-02 21:20:49 +0600

    done the single page

--- src/pages/Detailspage/Detail.jsx
+++ src/pages/Detailspage/Detail.jsx
@@ -2,13 +2,15 @@ import { useLoaderData } from "react-router-dom"
 import { apiKey, getPosterPath } from "../../constant/tmdbApiKey";
 import { useEffect, useState } from "react";
 import OneView from "../../components/OneView/OneView";
+import { HiStar } from "react-icons/hi2";
 
 const Detail = () => {
 
   const [img, setImg] = useState('')
-  const [similar,setSimilar]=useState([])
+  const [similar, setSimilar] = useState([])
 
   const x = useLoaderData()
+  console.log(x);
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${x.id}/images?api_key=${apiKey}`)
@@ -25,48 +27,49 @@ const Detail = () => {
   }, [similar]);
 
 
- 
+
 
   return (
     <div className=" overflow-y-scroll bgx h-screen w-full text-white " style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${img?.file_path})` }}>
-      <div className="bgn w-full flex flex-col p-10 ">
-        <div className="flex items-center flex-col lg:flex-row lg:space-x-10 my-20">
-          <div className="min-w-[300px] h-[400px]  rounded-lg bg-red-400 bgx" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500${x?.poster_path})` }}></div>
-          <div className="">
-            <h1>{x?.original_title}</h1>
-            <p>{x?.release_date} | {x?.runtime} min | 1080P-FHD | <span className=" font-bold">Total View</span> : {x?.vote_count}</p>
-            <p>{x?.overview}</p>
-            <h1>Extra info---------------</h1>
-            <p>Budget : {x?.budget}$</p>
-            <div className="flex space-x-5 bg-red-400">
+      <div className="bgn w-full flex flex-col lg:p-10 ">
+        <div className="flex justify-center items-center flex-col lg:flex-row lg:space-x-10 my-10 lg:my-20">
+          <div className="min-w-[300px] mb-10 lg:mb-0 h-[400px]  rounded-lg bg-red-400 bgx" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500${x?.poster_path})` }}></div>
+          <div className="px-5 lg:px-0">
+            <h1 className="text-5xl mb-5 font-bold max-w-2xl">{x?.original_title}</h1>
+            <p className="my-2">{x?.release_date} | {x?.runtime} min | 1080P-FHD | <span className=" font-bold">Total View</span> : {x?.vote_count}</p>
+            <p className="flex items-center"><HiStar className="text-3xl text-yellow-400 mb-2" /><span className="-mt-2 text-lg ml-1 font-bold">{x?.vote_average.toFixed(1)}</span></p>
+            <p><span className="font-semibold">Original Title : </span><span>{x?.original_title}</span></p>
+            <p className="my-2 text-gray-300 max-w-2xl">{x?.overview}</p>
+            <p className="mb-2"><span className="font-semibold">Budget : </span><span className="font-base">{x?.budget}$</span></p>
+            <div className="flex space-x-5 mb-2">
               <ul>
-                <li>Genres :</li>
-                {x.genres.map((one) => <li key={one.id}>{one.name}</li>)}
+                <li className="font-bold">Genres :</li>
+                {x.genres.map((one) => <li className="ml-5" key={one.id}>{one.name}</li>)}
               </ul>
               <ul>
-                <li>Genres :</li>
-                {x?.genres.map((one) => <li key={one.id}>{one.name}</li>)}
+                <li className="font-bold">Audio :</li>
+                {x?.spoken_languages.map((one) => <li className="ml-5" key={one.id}>{one.english_name}</li>)}
               </ul>
             </div>
-            <div className=" bg-blue-600">
-              <h1>Production Details</h1>
-              <h1>Country : {x.production_countries[0]?.name}</h1>
-              {x?.production_companies.map((one) => {
-                return (
-                  <div key={one?.id}>
-                    <p>{one?.name}</p>
-                  </div>
-                )
-              })}
+            <div className="ml-5">
+              <h1 className="-ml-5 font-bold">Production Details</h1>
+              <h1><span className="font-bold">Country :</span> {x.production_countries[0]?.name}</h1>
+              <div className="flex flex-wrap justify-bettween max-w-2xl">
+                {x?.production_companies.map((one) => {
+                  return (
+                    <p key={one?.id} className="font-semibold text-gray-300 mr-3">{one?.name},</p>
+                  )
+                })}
+              </div>
             </div>
-
+            <a href={x?.homepage} target="blank" className="btn btn-sm my-2">Get More Info</a>
           </div>
         </div>
         <div className="py-20">
-          <h1>You may like</h1>
-        <div className="flex gap-4 overflow-x-scroll flex-wrap justify-around">
-          {similar?.map((one)=><OneView key={one.id} data={one}/>)}
-        </div>
+          <h1 className="mb-20 font-bold text-5xl text-center">Related Movies</h1>
+          <div className="flex gap-4 overflow-x-scroll flex-wrap justify-around">
+            {similar?.map((one) => <OneView key={one.id} data={one} />)}
+          </div>
         </div>
       </div>
     </div>
